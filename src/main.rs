bfb520eb4527f76ec6ab70817c5ab515242b1c47
v3ctr4x <asurmendidelcampomikel@protonmail.com>
2025-04-04 13:22:41 +0200

    Add some options in network attack

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,33 @@ fn main() {
 fn network_attack() {
     println!("{}", "Network Attack selected!".green());
     
-}
+       // Esperar la entrada del usuario para continuar (esperando que presione Enter)
+       println!("{}", "Press Enter to continue to the Network Attack submenu...");
+       let mut wait = String::new();
+       io::stdin().read_line(&mut wait).unwrap();
+   
+       // Mostrar un submenú para Network Attack
+       println!("----------------------------");
+       println!("Network Attack Submenu:");
+       println!("1. Scan network (NMAP)");
+       println!("2. Perform DOS attack");
+       println!("3. Return to main menu");
+       println!("----------------------------");
+   
+       // Leer la opción del submenú
+       let mut sub_option = String::new();
+       io::stdin().read_line(&mut sub_option).unwrap();
+       let sub_option: i32 = sub_option.trim().parse().unwrap();
+   
+       
+       match sub_option {
+         //  1 => scan_network(),
+         //  2 => dos_attack(),
+         //  3 => return_to_main_menu(),
+           _ => println!("Invalid option! Please choose 1, 2, or 3."),
+       }
+   }
+
 
 fn exploit_searcher() {
     println!("{}", "Exploit searcher selected!".green());
